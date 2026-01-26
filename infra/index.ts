4ea955d4fee07675c70b2662b501aac2d8922e26
Kaivv1 <kvalchevv1@gmail.com>
2026-01-26 16:52:55 +0200

    wip

--- infra/index.ts
+++ infra/index.ts
@@ -14,46 +14,69 @@ const provider = new k8s.Provider(`${stack}-provider`, {
     namespace: stack,
 });
 
-const appLabels = { app: "nginx" };
-const nginxDeployment = new k8s.apps.v1.Deployment(
-    "nginx",
+const cert = new k8s.apiextensions.CustomResource(
+    "my-cert",
     {
+        apiVersion: "cert-manager.io/v1",
+        kind: "Certificate",
+        metadata: {
+            name: "my-cert",
+            namespace: "default",
+        },
         spec: {
-            selector: { matchLabels: appLabels },
-            replicas: 2,
-            template: {
-                metadata: { labels: appLabels },
-                spec: {
-                    containers: [
-                        {
-                            name: "nginx",
-                            image: "nginxdemos/hello:latest",
-                            ports: [
-                                {
-                                    containerPort: 80,
-                                },
-                            ],
-                        },
-                    ],
-                },
+            secretName: "my-cert-tls",
+            issuerRef: {
+                name: "letsencrypt-prod",
+                kind: "ClusterIssuer",
             },
+            dnsNames: ["traakt.com", "www.traakt.com"],
         },
     },
-    { provider, dependsOn: stackNamespace },
+    { provider },
 );
 
-const nginxService = new k8s.core.v1.Service(
-    "nginx-service",
-    {
-        spec: {
-            type: "NodePort",
-            selector: appLabels,
-            ports: [{ targetPort: 80, port: 80, nodePort: 30080 }],
-        },
-    },
-    { provider, dependsOn: stackNamespace },
-);
+// const appLabels = { app: "nginx" };
+// const nginxDeployment = new k8s.apps.v1.Deployment(
+//     "nginx",
+//     {
+//         spec: {
+//             selector: { matchLabels: appLabels },
+//             replicas: 2,
+//             template: {
+//                 metadata: { labels: appLabels },
+//                 spec: {
+//                     containers: [
+//                         {
+//                             name: "nginx",
+//                             image: "nginxdemos/hello:latest",
+//                             ports: [
+//                                 {
+//                                     containerPort: 80,
+//                                     name: "nginx-port",
+//                                 },
+//                             ],
+//                         },
+//                     ],
+//                 },
+//             },
+//         },
+//     },
+//     { provider, dependsOn: stackNamespace },
+// );
+
+// const nginxService = new k8s.core.v1.Service(
+//     "nginx-service",
+//     {
+//         spec: {
+//             type: "NodePort",
+//             selector: appLabels,
+//             ports: [{ targetPort: "nginx-port", port: 80 }],
+//         },
+//     },
+//     { provider, dependsOn: stackNamespace },
+// );
 
 export const stackNamespaceName = stackNamespace.metadata.name;
-export const deploymentName = nginxDeployment.metadata.name;
-export const serviceName = nginxService.metadata.name;
+export const certName = cert.metadata.name;
+// export const deploymentName = nginxDeployment.metadata.name;
+// export const serviceName = nginxService.metadata.name;
