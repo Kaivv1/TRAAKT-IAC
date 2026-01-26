d87e34056ae3b874c54a6584f1e31c34fa40c684
Kaivv1 <kvalchevv1@gmail.com>
2026-01-26 15:35:57 +0200

    wip

--- infra/index.ts
+++ infra/index.ts
@@ -4,25 +4,56 @@ import * as pulumi from "@pulumi/pulumi";
 const config = new pulumi.Config();
 const stack = pulumi.getStack();
 
-// const provider = new k8s.Provider("provider", {});
 const stackNamespace = new k8s.core.v1.Namespace(stack, {
     metadata: {
         name: stack,
         labels: { name: stack },
     },
 });
+const provider = new k8s.Provider(`${stack}-provider`, {
+    namespace: stack,
+});
+
+const appLabels = { app: "nginx" };
+const nginxDeployment = new k8s.apps.v1.Deployment(
+    "nginx",
+    {
+        spec: {
+            selector: { matchLabels: appLabels },
+            replicas: 2,
+            template: {
+                metadata: { labels: appLabels },
+                spec: {
+                    containers: [
+                        {
+                            name: "nginx",
+                            image: "nginxdemos/hello:latest",
+                            ports: [
+                                {
+                                    containerPort: 80,
+                                },
+                            ],
+                        },
+                    ],
+                },
+            },
+        },
+    },
+    { provider, dependsOn: stackNamespace },
+);
+
+const nginxService = new k8s.core.v1.Service(
+    "nginx-service",
+    {
+        spec: {
+            type: "NodePort",
+            selector: appLabels,
+            ports: [{ targetPort: 80, port: 80, nodePort: 30080 }],
+        },
+    },
+    { provider, dependsOn: stackNamespace },
+);
 
 export const stackNamespaceName = stackNamespace.metadata.name;
-
-// const appLabels = { app: "nginx" };
-// const deployment = new k8s.apps.v1.Deployment("nginx", {
-//     spec: {
-//         selector: { matchLabels: appLabels },
-//         replicas: 2,
-//         template: {
-//             metadata: { labels: appLabels },
-//             spec: { containers: [{ name: "nginx", image: "nginxdemos/hello:latest" }] },
-//         },
-//     },
-// });
-// export const name = deployment.metadata.name;
+export const deploymentName = nginxDeployment.metadata.name;
+export const serviceName = nginxService.metadata.name;
