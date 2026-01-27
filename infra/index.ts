59387eb674173b9489eb43a94b333eebb18d26c8
Kaivv1 <kvalchevv1@gmail.com>
2026-01-27 16:19:25 +0200

    wip

--- infra/index.ts
+++ infra/index.ts
@@ -125,6 +125,25 @@ const nginxService = new k8s.core.v1.Service(
 const issuer = stack === "prod" ? "letsencrypt-prod" : "letsencrypt-staging";
 const domains = stack === "prod" ? ["traakt.com", "www.traakt.com"] : [`${stack}.traakt.com`];
 
+const httpsRedirect = new k8s.apiextensions.CustomResource(
+    "https-redirect",
+    {
+        apiVersion: "traefik.containo.us/v1alpha1",
+        kind: "Middleware",
+        metadata: {
+            name: "https-redirect",
+            namespace: stack,
+        },
+        spec: {
+            redirectScheme: {
+                scheme: "https",
+                permanent: true,
+            },
+        },
+    },
+    { provider, dependsOn: stackNs },
+);
+
 const nginxIngress = new k8s.networking.v1.Ingress(
     "nginx-ingress",
     {
@@ -132,8 +151,7 @@ const nginxIngress = new k8s.networking.v1.Ingress(
             annotations: {
                 "kubernetes.io/ingress.class": "traefik",
                 "cert-manager.io/cluster-issuer": issuer,
-                "traefik.ingress.kubernetes.io/redirect-entry-point": "https",
-                "traefik.ingress.kubernetes.io/redirect-permanent": "true",
+                "traefik.ingress.kubernetes.io/router.middlewares": `${stack}-https-redirect@kubernetescrd`,
             },
         },
         spec: {
@@ -162,7 +180,7 @@ const nginxIngress = new k8s.networking.v1.Ingress(
             })),
         },
     },
-    { provider, dependsOn: [letsEncryptStaging, letsEncryptProd] },
+    { provider, dependsOn: [letsEncryptStaging, letsEncryptProd, httpsRedirect] },
 );
 
 export const nginxDeploymentName = nginxDeployment.metadata.name;
