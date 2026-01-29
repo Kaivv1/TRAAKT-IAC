5eab4af50ee031a644cde90a58a41ca40a39a8dc
Kaivv1 <kvalchevv1@gmail.com>
2026-01-30 00:16:53 +0200

    wip

--- infra/shared/config.ts
+++ infra/shared/config.ts
@@ -4,7 +4,7 @@ import type { DeploymentStack } from "./types";
 const config = new pulumi.Config();
 export const stack = pulumi.getStack() as DeploymentStack;
 
-export const issuer = "letsencrypt-test";
+export const issuer = "letsencrypt";
 export const domains = !["dev", "demo"].includes(stack) ? ["traakt.com", "www.traakt.com"] : [`${stack}.traakt.com`];
 
 export const labels = {
