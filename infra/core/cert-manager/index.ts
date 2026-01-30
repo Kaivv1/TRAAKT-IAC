b725cb5dee6af0ac7656fa16407b78d4828c3260
Kaivv1 <kvalchevv1@gmail.com>
2026-01-30 02:22:27 +0200

    wip

--- infra/core/cert-manager/index.ts
+++ infra/core/cert-manager/index.ts
@@ -5,11 +5,11 @@ import { waitForCertManager } from "./jobs";
 const info = {
     namespace: certManagerNs.metadata.name,
     clusterIssuers: {
-        letsEncrypt: letsEncrypt.metadata.name,
-        letsEncryptTest: letsEncryptTest.metadata.name,
+        letsEncrypt: letsEncrypt.metadata?.name,
+        letsEncryptTest: letsEncryptTest.metadata?.name,
     },
     jobs: {
-        waitForCertManager: waitForCertManager.metadata.name,
+        waitForCertManager: waitForCertManager.metadata?.name,
     },
     certManagerChart: "cert-manager",
 };
