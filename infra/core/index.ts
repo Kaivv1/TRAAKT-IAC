ebe8beec83427f8062f43627fbec8a50507a33a1
Kaivv1 <kvalchevv1@gmail.com>
2026-02-08 02:29:26 +0200

    wip

--- infra/core/index.ts
+++ infra/core/index.ts
@@ -1,8 +1,6 @@
-import { certManager, certManagerNs } from "./cert-manager/cert-manager";
+import { certManagerNs } from "./cert-manager/cert-manager";
 import { letsEncrypt, letsEncryptTest } from "./cert-manager/cluster-issuers";
-import { waitForCertManager } from "./cert-manager/jobs";
 import { certificate } from "./cert-manager/certificate";
-import { reflector } from "./cert-manager/reflector";
 
 export const deployed = {
     namespace: certManagerNs.metadata.name,
@@ -11,9 +9,6 @@ export const deployed = {
     clusterIssuers: {
         letsEncrypt: letsEncrypt.metadata.name,
         letsEncryptTest: letsEncryptTest.metadata.name,
-    },
-    jobs: {
-        waitForCertManager: waitForCertManager.metadata.name,
     },
     certificate: certificate.metadata.name,
 };
