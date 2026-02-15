c482e0171ef4553e036aac1fb208cbdf835616d9
Kaivv1 <kvalchevv1@gmail.com>
2026-02-15 11:35:17 +0200

    wip

--- infra/core-infra/vault/index.ts
+++ infra/core-infra/vault/index.ts
@@ -1,4 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as command from "@pulumi/command";
 import { createVaultNs } from "./namespace";
 import { createVaultIngress } from "./ingress";
 import { createVaultChart } from "./vault";
@@ -9,7 +10,14 @@ export const deployVault = (dependsOn: pulumi.Resource[]) => {
     const vaultNs = createVaultNs(dependsOn);
     const vaultChart = createVaultChart(vaultNs.metadata.name, [vaultNs]);
     const vaultIngress = createVaultIngress(vaultNs.metadata.name, [vaultChart]);
-    const { initStoragePod } = createPersistentFilePod(vaultNs.metadata.name, [vaultIngress]);
+    const vaultPodCheck = new command.local.Command(
+        "check-vault-pod",
+        {
+            create: `kubectl wait --for=jsonpath='{.status.phase}'=Running -n vault vault-0 --timeout=3m`,
+        },
+        { dependsOn: [vaultIngress] },
+    );
+    const { initStoragePod } = createPersistentFilePod(vaultNs.metadata.name, [vaultPodCheck]);
     const unsealVaultsCommand = createUnsealVaultsCommand([initStoragePod]);
 
     return { vaultNs, vaultChart, vaultIngress, unsealVaultsCommand };
