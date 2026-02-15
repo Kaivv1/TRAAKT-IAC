a7f3bae9a4178871360a1dfb983b5c294fd4ebde
Kaivv1 <kvalchevv1@gmail.com>
2026-02-15 14:33:10 +0200

    wip

--- infra/core-infra/vault/commands/configure-command.ts
+++ infra/core-infra/vault/commands/configure-command.ts
@@ -13,24 +13,31 @@ export const createConfigureVaultCommand = (dependsOn: pulumi.Resource[]) => {
                 kubectl exec vault-0 -n vault -- sh -c "
                 export VAULT_TOKEN='$ROOT_TOKEN'
                 
-                vault secrets enable -path=secret kv-v2 || true
                 vault auth enable kubernetes || true
-                vault write auth/kubernetes/config kubernetes_host='https://\$KUBERNETES_PORT_443_TCP_ADDR:443'
+                vault secrets enable -path=secret kv-v2 || true
 
+                vault write auth/kubernetes/config \
+                    kubernetes_host='https://\$KUBERNETES_PORT_443_TCP_ADDR:443' \
+                    token_reviewer_jwt=@/var/run/secrets/kubernetes.io/serviceaccount/token \
+                    kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+                
                 vault policy write backend-policy /tmp/vault-policy.hcl
+                
+                vault kv put secret/backend/config placeholder=true
 
                 vault write auth/kubernetes/role/backend-dev \
                     bound_service_account_names=backend \
                     bound_service_account_namespaces=backend-service-dev \
                     policies=backend-policy \
                     ttl=24h
-
+                
                 vault write auth/kubernetes/role/backend-demo \
                     bound_service_account_names=backend \
                     bound_service_account_namespaces=backend-service-demo \
                     policies=backend-policy \
                     ttl=24h
                 "
+                
             `,
         },
         { dependsOn },
