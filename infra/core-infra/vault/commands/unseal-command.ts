1a416137d9b9eaa936c037c1da0712a494676705
Kaivv1 <kvalchevv1@gmail.com>
2026-03-01 19:12:13 +0200

    toggle vault

--- infra/core-infra/vault/commands/unseal-command.ts
+++ infra/core-infra/vault/commands/unseal-command.ts
@@ -6,34 +6,30 @@ export const createUnsealVaultsCommand = (dependsOn: pulumi.Resource[]) => {
         "unseal-vaults",
         {
             create: `
-            kubectl wait --for=condition=Ready -n vault pod/init-pod --timeout=3m
+                kubectl wait --for=condition=Ready -n vault pod/init-pod --timeout=3m
 
-            sleep 15
-            kubectl cp vault/init-pod:/vault-init/vault-init.json ./vault-init.json
-            sleep 2
+                sleep 15
+                kubectl cp vault/init-pod:/vault-init/vault-init.json ./vault-init.json
+                sleep 2
 
-            KEY1=$(cat vault-init.json | jq -r '.unseal_keys_b64[0]')
-            KEY2=$(cat vault-init.json | jq -r '.unseal_keys_b64[1]')
-            KEY3=$(cat vault-init.json | jq -r '.unseal_keys_b64[2]')
+                KEY1=$(cat vault-init.json | jq -r '.unseal_keys_b64[0]')
+                KEY2=$(cat vault-init.json | jq -r '.unseal_keys_b64[1]')
+                KEY3=$(cat vault-init.json | jq -r '.unseal_keys_b64[2]')
 
-            kubectl exec -n vault vault-0 -- sh -c 'VAULT_CACERT=/vault/userconfig/vault-tls/ca.crt vault operator unseal "$1"' -- "$KEY1"
-            kubectl exec -n vault vault-0 -- sh -c 'VAULT_CACERT=/vault/userconfig/vault-tls/ca.crt vault operator unseal "$1"' -- "$KEY2"
-            kubectl exec -n vault vault-0 -- sh -c 'VAULT_CACERT=/vault/userconfig/vault-tls/ca.crt vault operator unseal "$1"' -- "$KEY3"
+                kubectl exec -n vault vault-0 -- sh -c 'VAULT_CACERT=/vault/userconfig/vault-tls/ca.crt vault operator unseal "$1"' -- "$KEY1"
+                kubectl exec -n vault vault-0 -- sh -c 'VAULT_CACERT=/vault/userconfig/vault-tls/ca.crt vault operator unseal "$1"' -- "$KEY2"
+                kubectl exec -n vault vault-0 -- sh -c 'VAULT_CACERT=/vault/userconfig/vault-tls/ca.crt vault operator unseal "$1"' -- "$KEY3"
 
-            sleep 15
+                kubectl wait --for=condition=Initialized -n vault pod/vault-1 --timeout=3m
+                kubectl exec -n vault vault-1 -- sh -c 'VAULT_CACERT=/vault/userconfig/vault-tls/ca.crt vault operator unseal "$1"' -- "$KEY1"
+                kubectl exec -n vault vault-1 -- sh -c 'VAULT_CACERT=/vault/userconfig/vault-tls/ca.crt vault operator unseal "$1"' -- "$KEY2"
+                kubectl exec -n vault vault-1 -- sh -c 'VAULT_CACERT=/vault/userconfig/vault-tls/ca.crt vault operator unseal "$1"' -- "$KEY3"
 
-            kubectl exec -n vault vault-1 -- sh -c 'VAULT_CACERT=/vault/userconfig/vault-tls/ca.crt vault operator unseal "$1"' -- "$KEY1"
-            kubectl exec -n vault vault-1 -- sh -c 'VAULT_CACERT=/vault/userconfig/vault-tls/ca.crt vault operator unseal "$1"' -- "$KEY2"
-            kubectl exec -n vault vault-1 -- sh -c 'VAULT_CACERT=/vault/userconfig/vault-tls/ca.crt vault operator unseal "$1"' -- "$KEY3"
-
-            sleep 15
-
-            kubectl exec -n vault vault-2 -- sh -c 'VAULT_CACERT=/vault/userconfig/vault-tls/ca.crt vault operator unseal "$1"' -- "$KEY1"
-            kubectl exec -n vault vault-2 -- sh -c 'VAULT_CACERT=/vault/userconfig/vault-tls/ca.crt vault operator unseal "$1"' -- "$KEY2"
-            kubectl exec -n vault vault-2 -- sh -c 'VAULT_CACERT=/vault/userconfig/vault-tls/ca.crt vault operator unseal "$1"' -- "$KEY3"
-
-            sleep 15
-        `,
+                kubectl wait --for=condition=Initialized -n vault pod/vault-2 --timeout=3m
+                kubectl exec -n vault vault-2 -- sh -c 'VAULT_CACERT=/vault/userconfig/vault-tls/ca.crt vault operator unseal "$1"' -- "$KEY1"
+                kubectl exec -n vault vault-2 -- sh -c 'VAULT_CACERT=/vault/userconfig/vault-tls/ca.crt vault operator unseal "$1"' -- "$KEY2"
+                kubectl exec -n vault vault-2 -- sh -c 'VAULT_CACERT=/vault/userconfig/vault-tls/ca.crt vault operator unseal "$1"' -- "$KEY3"
+            `,
         },
         { dependsOn },
     );
