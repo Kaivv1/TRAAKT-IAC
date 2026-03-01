97ff94b1066f9ffc5e9f6b30e9472d737e16efe9
Kaivv1 <kvalchevv1@gmail.com>
2026-03-01 13:09:08 +0200

    wip

--- infra/core-infra/vault/index.ts
+++ infra/core-infra/vault/index.ts
@@ -14,7 +14,7 @@ export const deployVault = (dependsOn: pulumi.Resource[]) => {
     const vaultPodCheck = new command.local.Command(
         "check-vault-pod",
         {
-            create: `kubectl wait --for=jsonpath='{.status.phase}'=Running -n vault pod/vault-0 --timeout=3m`,
+            create: `kubectl wait --for=condition=Initialized pod/vault-0 -n vault --timeout=5m`,
         },
         { dependsOn: [vaultIngress] },
     );
