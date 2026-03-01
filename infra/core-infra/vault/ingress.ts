f3901727e6076599191044a880b4c584c054614c
Kaivv1 <kvalchevv1@gmail.com>
2026-03-01 17:30:32 +0200

    wip

--- infra/core-infra/vault/ingress.ts
+++ infra/core-infra/vault/ingress.ts
@@ -24,7 +24,7 @@ export const createVaultIngress = (namespace: pulumi.Output<string>, dependsOn: 
                 annotations: {
                     "kubernetes.io/ingress.class": "traefik",
                     "traefik.ingress.kubernetes.io/router.middlewares": middlewaresLiteral,
-                    "traefik.ingress.kubernetes.io/service.serverstransport": "vault-transport@vault",
+                    "traefik.ingress.kubernetes.io/service.serverstransport": "vault-transport@kube-system",
                 },
             },
             spec: {
