2e5780c9e219c805495edfd80639843bd1f2cec4
Kaivv1 <kvalchevv1@gmail.com>
2026-03-01 18:52:04 +0200

    toggle vault

--- infra/core-infra/vault/ingress.ts
+++ infra/core-infra/vault/ingress.ts
@@ -14,49 +14,100 @@ export const createVaultIngress = (namespace: pulumi.Output<string>, dependsOn: 
 
     const middlewaresLiteral = pulumi.interpolate`${namespace}-${vaultHttpsRedirectMiddleware.name}${crd}`;
 
-    return new k8s.networking.v1.Ingress(
-        `vault-ingress`,
+    // return new k8s.networking.v1.Ingress(
+    //     `vault-ingress`,
+    //     {
+    //         metadata: {
+    //             name: `vault-ingress`,
+    //             namespace,
+    //             labels,
+    //             annotations: {
+    //                 "kubernetes.io/ingress.class": "traefik",
+    //                 "traefik.ingress.kubernetes.io/router.middlewares": middlewaresLiteral,
+    //                 "traefik.ingress.kubernetes.io/service.serverstransport": "vault-transport@vault",
+    //             },
+    //         },
+    //         spec: {
+    //             tls: [
+    //                 {
+    //                     hosts: [`vault.traakt.com`],
+    //                     secretName: "tls-cert-secret",
+    //                 },
+    //             ],
+    //             rules: [
+    //                 {
+    //                     host: `vault.traakt.com`,
+    //                     http: {
+    //                         paths: [
+    //                             {
+    //                                 path: "/",
+    //                                 pathType: "Prefix",
+    //                                 backend: {
+    //                                     service: {
+    //                                         name: "vault",
+    //                                         port: { number: 8200 },
+    //                                     },
+    //                                 },
+    //                             },
+    //                         ],
+    //                     },
+    //                 },
+    //             ],
+    //         },
+    //     },
+    //     {
+    //         dependsOn: [vaultHttpsRedirectMiddleware, ...dependsOn],
+    //     },
+    // );
+    const httpRoute = new k8s.apiextensions.CustomResource(
+        "vault-ingress-http",
         {
-            metadata: {
-                name: `vault-ingress`,
-                namespace,
-                labels,
-                annotations: {
-                    "kubernetes.io/ingress.class": "traefik",
-                    "traefik.ingress.kubernetes.io/router.middlewares": middlewaresLiteral,
-                    "traefik.ingress.kubernetes.io/service.serverstransport": "vault-transport@vault",
+            apiVersion: "traefik.io/v1alpha1",
+            kind: "IngressRoute",
+            metadata: { name: "vault-ingress-http", namespace, labels },
+            spec: {
+                entryPoints: ["web"],
+                routes: [
+                    {
+                        match: "Host(`vault.traakt.com`)",
+                        kind: "Rule",
+                        middlewares: [{ name: vaultHttpsRedirectMiddleware.name, namespace }],
+                        services: [{ name: "vault", port: 8200 }],
+                    },
+                ],
+            },
+        },
+        { dependsOn: [vaultHttpsRedirectMiddleware, ...dependsOn] },
+    );
+
+    const httpsRoute = new k8s.apiextensions.CustomResource(
+        "vault-ingress-https",
+        {
+            apiVersion: "traefik.io/v1alpha1",
+            kind: "IngressRoute",
+            metadata: { name: "vault-ingress-https", namespace, labels },
+            spec: {
+                entryPoints: ["websecure"],
+                routes: [
+                    {
+                        match: "Host(`vault.traakt.com`)",
+                        kind: "Rule",
+                        services: [
+                            {
+                                name: "vault",
+                                port: 8200,
+                                serversTransport: "vault-transport",
+                            },
+                        ],
+                    },
+                ],
+                tls: {
+                    secretName: "tls-cert-secret",
                 },
             },
-            spec: {
-                tls: [
-                    {
-                        hosts: [`vault.traakt.com`],
-                        secretName: "tls-cert-secret",
-                    },
-                ],
-                rules: [
-                    {
-                        host: `vault.traakt.com`,
-                        http: {
-                            paths: [
-                                {
-                                    path: "/",
-                                    pathType: "Prefix",
-                                    backend: {
-                                        service: {
-                                            name: "vault",
-                                            port: { number: 8200 },
-                                        },
-                                    },
-                                },
-                            ],
-                        },
-                    },
-                ],
-            },
         },
-        {
-            dependsOn: [vaultHttpsRedirectMiddleware, ...dependsOn],
-        },
+        { dependsOn: [vaultHttpsRedirectMiddleware, ...dependsOn] },
     );
+
+    return httpsRoute;
 };
