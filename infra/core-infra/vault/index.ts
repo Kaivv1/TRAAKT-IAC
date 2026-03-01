ac9df6f7a26dc336f4d53f157ac440157c92162b
Kaivv1 <kvalchevv1@gmail.com>
2026-03-01 15:36:30 +0200

    wip

--- infra/core-infra/vault/index.ts
+++ infra/core-infra/vault/index.ts
@@ -14,9 +14,12 @@ export const deployVault = (dependsOn: pulumi.Resource[]) => {
     const vaultPodCheck = new command.local.Command(
         "check-vault-pod",
         {
-            create: `kubectl wait --for=condition=Initialized pod/vault-0 -n vault --timeout=5m`,
+            create: `
+                kubectl rollout status statefulset/vault -n vault --timeout=5m
+                kubectl wait --for=condition=Initialized pod/vault-0 -n vault --timeout=5m
+            `,
         },
-        { dependsOn: [vaultIngress] },
+        { dependsOn: [vaultChart] },
     );
     const { initStoragePod } = createPersistentFilePod(vaultNs.metadata.name, [vaultPodCheck]);
     const unsealVaultsCommand = createUnsealVaultsCommand([initStoragePod]);
